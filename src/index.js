297527958e263a0cdaae623e723ba42d1f680ab6
ChasKane <LawrenceCKane@gmail.com>
2018-11-30 12:39:08 -0500

    login actually doesn't work fully -- looking into why

--- src/index.js
+++ src/index.js
@@ -10,24 +10,31 @@ function login() {
     URL,
     {
       cmd:"gt",
-      user:,
-      password:"CDE50A071FD29CE8EBF9813061A2DB77"
+      user:$("#usernameInput").text() || DEFAULT_CREDS.user,
+      password:$("#passwordInput").text() || DEFAULT_CREDS.password
     },
     function(data) {
       sessionStorage.setItem("token", JSON.parse(data).parm3);
       sessionStorage.setItem("user", JSON.parse(data).parm4);
       sessionStorage.setItem("franchise", JSON.parse(data).parm19);
+
+      getStatus(JSON.parse(data).parm3, JSON.parse(data).parm19);
+
+      window.location = "scoreView.html";
     }
   );
+}
+
+function getStatus(token, franchise) {
   $.get(
     URL,
     {
       cmd:"gs",
-      token:sessionStorage.getItem("token"),
-      franchise:sessionStorage.getItem("franchise")
+      token:token,
+      franchise:franchise
     },
     function(data) {
-      window.location = "scoreView.html";
+      sessionStorage.setItem("franchise", JSON.parse(data).parm19);
     }
   );
 }
