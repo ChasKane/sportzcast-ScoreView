6be098491413dd22d08fb34391c0dc73c7c59865
YoungSage <LawrenceCKane@gmail.com>
2018-12-03 13:38:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var port = process.env.PORT || 1337;
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname+"/src/")).listen(1337, function(){
-    console.log('Server running on 1337...');
+connect().use(serveStatic(__dirname+"/src/")).listen(port, function(){
+    console.log('Server running on '+port);
 });
